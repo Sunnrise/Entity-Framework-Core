4f135b856041c7f43d2388614b76febe0e42eded
Alperen Güneş <alperenmanavgat@outlook.com>
2024-05-21 22:03:04 +0300

    Entries Method

--- Change_Tracker/Program.cs
+++ Change_Tracker/Program.cs
@@ -43,6 +43,40 @@ ECommerceDbContext context = new();
 #endregion
 
 #region Entries Method
+//The version of collection, Entry method which belongs to Context class.
+//Entity instances are tracked by the context and the state of the entity is stored in the EntityEntry object.
+//Entries method trigger the DetectChanges method to detect the changes made to the objects being tracked by the context. It causes some performance overhead.
+//So, if you want to manually detect the changes, you can use the DetectChanges method of the Change Tracker.
+
+var products =await context.Products.ToListAsync();
+products.FirstOrDefault(u=>u.Id==7).Price = 2001;
+context.Products.Remove(products.FirstOrDefault(u => u.Id == 8)); // delete the product at index 7
+products.FirstOrDefault(u => u.Id == 9).ProductName = "Sunnrise";// update the product at index 3
+
+context.ChangeTracker.Entries().ToList().ForEach(e=>
+{
+    if (e.State==EntityState.Unchanged)
+    {
+
+    }
+    else if (e.State == EntityState.Added)
+    {
+
+    }
+    else if (e.State == EntityState.Modified)
+    {
+
+    }
+    else if (e.State == EntityState.Deleted)
+    {
+
+    }
+    else if (e.State == EntityState.Detached)
+    {
+
+    }
+
+}); //Returns all the entities being tracked by the context
 
 #endregion
 
