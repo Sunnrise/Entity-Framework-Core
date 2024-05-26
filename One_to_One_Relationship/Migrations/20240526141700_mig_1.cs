53910eb9d114e651729167807a6327eb8c27b0a6
Alperen Güneş <alperenmanavgat@outlook.com>
2024-05-26 23:26:55 +0300

    Default Convention

---
+++ One_to_One_Relationship/Migrations/20240526141700_mig_1.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace One_to_One_Relationship.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EmployeeAddresses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    EmployeeID = table.Column<int>(type: "int", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeAddresses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EmployeeAddresses_Employees_Id",
+                        column: x => x.Id,
+                        principalTable: "Employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmployeeAddresses");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
---
+++ One_to_One_Relationship/Program.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection.Emit;
+
+ECommerceDbContext context = new();
+
+#region Default Convention
+
+class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public EmployeeAddress EmployeeAddress { get; set; }
+}
+class EmployeeAddress
+{
+    public int Id { get; set; }//Primary Key
+    public int EmployeeId { get; set; }//Foreign Key
+    public string Address { get; set; }
+
+    public Employee Employee { get; set; }
+}
+#endregion
+#region Data Annotations
+
+//class Employee
+//{
+//    public int Id { get; set; }
+//    public string Name { get; set; }
+
+//    public EmployeeAddress EmployeeAddress { get; set; }
+//}
+//class EmployeeAddress
+//{
+//    [Key, ForeignKey(nameof(Employee))]
+//    public int Id { get; set; }
+//    public string Address { get; set; }
+
+//    public Employee Employee { get; set; }
+//}
+#endregion
+#region Fluent API
+
+//class Employee
+//{
+//    public int Id { get; set; }
+//    public string Name { get; set; }
+
+//    public EmployeeAddress EmployeeAddress { get; set; }
+//}
+//class EmployeeAddress
+//{
+//    public int Id { get; set; }
+//    public string Address { get; set; }
+
+//    public Employee Employee { get; set; }
+//}
+#endregion
+class ECommerceDbContext : DbContext
+{
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<EmployeeAddress> EmployeeAddresses { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost, 1433;Database=ECommerceDb;User Id=sa;Password=Password1;TrustServerCertificate=True");
+    }
+   
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<EmployeeAddress>()
+            .HasKey(c => c.Id);
+
+        modelBuilder.Entity<Employee>()
+             .HasOne(c => c.EmployeeAddress)
+             .WithOne(c => c.Employee)
+             .HasForeignKey<EmployeeAddress>(c => c.Id);
+    }
+}
