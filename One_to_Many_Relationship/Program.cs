215521ae0ce2a0a0036c97387ffc90f4ad072ab6
Alperen Güneş <alperenmanavgat@outlook.com>
2024-05-28 18:59:31 +0300

    One to many project creation

---
+++ One_to_Many_Relationship/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+ECommerceDbContext context = new();
+
+#region Default Convention
+
+class Employee //Dependent Entity
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+class Department
+{
+    public int Id { get; set; }
+    public string DepartmentName { get; set; }
+}
+#endregion
+#region Data Annotations
+
+
+
+#endregion
+#region Fluent API
+
+#endregion
+
+
+class ECommerceDbContext : DbContext
+{
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Department> Departments { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost, 1433;Database=ECommerceDb;User Id=sa;Password=Password1;TrustServerCertificate=True");
+    }
+
+
+}
