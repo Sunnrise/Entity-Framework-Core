1ad0bcdd1ef18dce91b6da49390a72b1d90e504c
Alperen Güneş <alperenmanavgat@outlook.com>
2024-05-29 11:42:18 +0300

    Data Annotations

--- One_to_Many_Relationship/Program.cs
+++ One_to_Many_Relationship/Program.cs
@@ -4,13 +4,33 @@ using System.ComponentModel.DataAnnotations.Schema;
 ECommerceDbContext context = new();
 
 #region Default Convention
-//in Default Convention, EF Core will create a foreign key column in the dependent entity (Employee) with the name DepartmentId (Navigation Property Name + Primary Key Name), if we don't specify the foreign key explicitly.
+////in Default Convention, EF Core will create a foreign key column in the dependent entity (Employee) with the name DepartmentId (Navigation Property Name + Primary Key Name), if we don't specify the foreign key explicitly.
+//class Employee //Dependent Entity 
+//{
+//    public int Id { get; set; }
+//    public int DepartmentId { get; set; } //Foreign Key
+//    public string Name { get; set; }
+
+//    public Department Department { get; set; } //Navigation Property
+//}
+//class Department
+//{
+//    public int Id { get; set; }
+//    public string DepartmentName { get; set; }
+
+//    public ICollection<Employee> Employees { get; set; } //Navigation Property
+//}
+#endregion
+#region Data Annotations
+//We can use the ForeignKey attribute to specify the foreign key property in the dependent entity (Employee) explicitly.
+// The ForeignKey attribute is applied to the navigation property in the dependent entity (Employee) and specifies the foreign key property (DId) in the dependent entity (Employee).
 class Employee //Dependent Entity 
 {
     public int Id { get; set; }
-    public int DepartmentId { get; set; } //Foreign Key
+    [ForeignKey(nameof(Department))]
+    public int DId { get; set; } //Foreign Key
     public string Name { get; set; }
-    
+
     public Department Department { get; set; } //Navigation Property
 }
 class Department
@@ -20,9 +40,6 @@ class Department
 
     public ICollection<Employee> Employees { get; set; } //Navigation Property
 }
-#endregion
-#region Data Annotations
-
 
 
 #endregion
