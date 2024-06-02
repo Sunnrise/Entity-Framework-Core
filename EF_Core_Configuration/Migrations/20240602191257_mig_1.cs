666c82f1bfa0d92500e83ca71b3eb297d69b4343
Alperen Güneş <alperenmanavgat@outlook.com>
2024-06-02 22:15:22 +0300

    Configuration

---
+++ EF_Core_Configuration/Migrations/20240602191257_mig_1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EF_Core_Configuration.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Departments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departments", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DepartmentId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Salary = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DepartmentId1 = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Persons_Departments_DepartmentId1",
+                        column: x => x.DepartmentId1,
+                        principalTable: "Departments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Persons_DepartmentId1",
+                table: "Persons",
+                column: "DepartmentId1");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Persons");
+
+            migrationBuilder.DropTable(
+                name: "Departments");
+        }
+    }
+}
---
+++ EF_Core_Configuration/Program.cs
@@ -0,0 +1,176 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+ApplicationDbContext context = new ApplicationDbContext();
+#region Why do we need Configuration in EF Core
+
+#endregion
+
+#region OnModelCreating Method
+
+#endregion
+#region Configurations | Data Annotations & Fluent API
+#region Table - ToTable
+
+#endregion
+
+#region Column - HasColumnName, HasColumnType, HasColumOrder
+
+#endregion
+
+#region ForeingKey - HasForeignKey
+
+#endregion
+
+#region NotMapped - Ignore
+
+#endregion
+
+#region Key - HasKey
+
+#endregion
+
+#region Timestamp - IsRowVersion
+
+#endregion
+
+#region Required - IsRequired
+
+#endregion
+
+#region MaxLength - HasMaxLength
+
+#endregion
+
+#region StringLength - HasMaxLength
+
+#endregion
+
+#region Precision - HasPrecision
+#endregion
+
+#region Unicode - IsUnicode
+#endregion
+
+#region Comment - HasComment
+#endregion
+
+#region ConcurrencyCheck - IsConcurrencyToken
+
+#endregion
+
+#region InverseProperty
+
+#endregion
+
+#endregion
+
+#region Configurations| Fluent API
+
+#region Composite Key
+#endregion
+
+#region HasDefaultSchema
+
+#endregion
+
+#region Property
+
+#region HasDefaultValue
+
+#endregion
+
+#region HasDefaultValueSql
+
+#endregion
+
+#endregion
+
+#region HasComputedColumnSql
+#endregion
+
+#region HasConstraintName
+
+#endregion
+
+#region HasData
+#endregion
+
+#region HasDiscriminator
+#endregion
+
+#region HasField
+#endregion
+
+#region HasIndex
+#endregion
+
+#region HasIndex
+#endregion
+
+#region HasQueryFilter
+#endregion
+
+#region HasValue
+#endregion
+
+#region DatabaseGenerated - ValueGeneratedOnAddOrUpdate, ValueGeneratedOnAdd, ValueGeneratedNever
+#endregion
+
+#endregion
+
+class Person
+{
+    public int Id { get; set; }
+    public string DepartmentId { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Salary { get; set; }
+
+    public DateTime CreatedDate { get; set; }
+    public Department Department { get; set; }
+}
+class Department
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public ICollection<Person> Persons { get; set; }
+}
+class ApplicationDbContext: DbContext
+{
+    public DbSet<Person> Persons { get; set; }
+    public DbSet<Department> Departments { get; set; }
+    //public Dbset<Flight> Flights { get; set; }
+    //public Dbset<Airport> Airports { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost, 1433;Database=ApplicationDb;User Id=sa;Password=Password1;TrustServerCertificate=True");
+    }  
+}
+public class Flight
+{
+    public int FlightID { get; set; }
+    public int DepartureAirportId { get; set; }
+    public int ArrivalAirportId { get; set; }
+    public string Name { get; set; }
+    public Airport DepartureAirport { get; set; }
+    public Airport ArrivalAirport { get; set; }
+}
+public class Airport
+{
+    public int AirportID { get; set; }
+    public string Name { get; set; }
+    [InverseProperty(nameof(Flight.DepartureAirport))]
+    public virtual ICollection<Flight> DepartingFlights { get; set; }
+
+    [InverseProperty(nameof(Flight.ArrivalAirport))]
+    public virtual ICollection<Flight> ArrivingFlights { get; set; }
+}
+
+
+
