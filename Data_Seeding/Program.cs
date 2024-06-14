36a70060eeb052e746a15724fb44d09587bc71e4
Alperen Güneş <alperenmanavgat@outlook.com>
2024-06-14 12:39:51 +0300

    SeedDataProject

---
+++ Data_Seeding/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+
+#region What is the DataSeeding
+#endregion
+
+#region Adding Data Seed
+#endregion
+
+#region Adding Data Seed for Related Entities
+
+#endregion
+
+#region Changing the Primary Key of the Seed Data
+#endregion
+
+class Post
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public int BlogId { get; set; }
+    public Blog Blog { get; set; }
+}
+class Blog
+{
+    public int Id { get; set; }
+    public string Url { get; set; }
+    public ICollection<Post> Posts { get; set; }
+}
+class ApplicationDbContext : DbContext
+{
+    public DbSet<Post> Posts { get; set; }
+    public DbSet<Blog> Blogs { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    { 
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost, 1433;Database=ApplicationDb;User Id=sa;Password=Password1;TrustServerCertificate=True");
+    }
+}
