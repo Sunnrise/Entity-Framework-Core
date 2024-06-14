5de353d2ead9e79ab2dca3d48900a65ecdb3247b
Alperen Güneş <alperenmanavgat@outlook.com>
2024-06-14 12:33:02 +0300

    Separation Entity Classes

---
+++ Separate_Configuration_Classes/Migrations/20240614092234_mig1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Separate_Configuration_Classes.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    OrderId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    OrderDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.OrderId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
---
+++ Separate_Configuration_Classes/Program.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Reflection;
+Console.WriteLine();
+#region OnModelCreating
+//We can configure the entity using the OnModelCreating method of the DbContext class.
+#endregion
+
+#region IEntityTypeConfiguration<T> Interface
+//We can create a separate class that implements the IEntityTypeConfiguration<T> interface for each entity.
+//We separate the configuration of each entity into a separate class. So we can manage the configuration of each entity separately.
+//If the entity configuration is complex, we can manage it in a separate class. It makes the code clean and easy to manage.
+#endregion
+
+#region ApplyConfiguration Method
+//We can apply the configuration to the entity using the ApplyConfiguration method of the ModelBuilder class.
+
+#endregion
+
+#region ApplyConfigurationsFromAssembly Method
+// We can apply the configuration to the entity using the ApplyConfigurationsFromAssembly method of the ModelBuilder class.
+// It applies all the configurations that are defined in the assembly.
+//We dont have to call the ApplyConfiguration method for each entity configuration.
+#endregion
+
+class Order
+{
+    public int OrderId { get; set; }
+    public string Description { get; set; }
+    public DateTime OrderDate { get; set; }
+}
+class OrderConfiguration:IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        builder.HasKey(o => o.OrderId);
+        builder.Property(o => o.Description)
+            .HasMaxLength(100)
+            .IsRequired();
+        builder.Property(o => o.OrderDate)
+            .HasDefaultValueSql("GETDATE()");
+    }
+}
+
+class ApplicationDbContext : DbContext
+{
+    public DbSet<Order> Orders { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //modelBuilder.ApplyConfiguration(new OrderConfiguration());
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost, 1433;Database=ApplicationDb;User Id=sa;Password=Password1;TrustServerCertificate=True");
+    }
+}
