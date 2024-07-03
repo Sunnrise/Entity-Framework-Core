e2e7ebdc7a07125bbd542eb526d79d9f918e888c
Alperen Güneş <alperenmanavgat@outlook.com>
2024-07-03 14:39:39 +0300

    Reference for singular

--- Loading_Related_Data_Explicit_Loading/Configurations/Datas.cs
+++ Loading_Related_Data_Explicit_Loading/Configurations/Datas.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,49 @@ using System.Threading.Tasks;
 
 namespace Loading_Related_Data_Explicit_Loading.Configurations
 {
-    internal class Datas
+    public class EmployeeData : IEntityTypeConfiguration<Employee>
     {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            builder.HasData(new Employee[]
+            {
+                new(){ Id = 1, RegionId=1, Name= "Alperen", Surname="Güneş", Salary= 1500,},
+                new(){ Id = 2, RegionId=2,Name= "Mehmet", Surname="Yılmaz", Salary= 2000},
+                new(){ Id = 3, RegionId=3,Name= "Ayşe", Surname="Kara", Salary= 2500},
+                new(){ Id = 4, RegionId=4,Name= "Fatma", Surname="Kara", Salary= 3000},
+
+            });
+        }
+    }
+    public class OrderData : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.HasData(new Order[]
+            {
+                new(){ Id = 1, OrderDate= DateTime.Now, EmployeeId= 1},
+                new(){ Id = 2, OrderDate= DateTime.Now, EmployeeId= 1},
+                new(){ Id = 3, OrderDate= DateTime.Now, EmployeeId= 2},
+                new(){ Id = 4, OrderDate= DateTime.Now, EmployeeId= 2},
+                new(){ Id = 5, OrderDate= DateTime.Now, EmployeeId= 3},
+                new(){ Id = 6, OrderDate= DateTime.Now, EmployeeId= 3},
+                new(){ Id = 7, OrderDate= DateTime.Now, EmployeeId= 4},
+                new(){ Id = 8, OrderDate= DateTime.Now, EmployeeId= 4},
+            });
+        }
+    }
+
+    public class RegionData : IEntityTypeConfiguration<Region>
+    {
+        public void Configure(EntityTypeBuilder<Region> builder)
+        {
+            builder.HasData(new Region[]
+            {
+                new(){ Id = 1, Name= "Marmara"},
+                new(){ Id = 2, Name= "Ege"},
+                new(){ Id = 3, Name= "Karadeniz"},
+                new(){ Id = 4, Name= "Akdeniz"},
+            });
+        }
     }
 }
--- Loading_Related_Data_Explicit_Loading/Program.cs
+++ Loading_Related_Data_Explicit_Loading/Program.cs
@@ -1,110 +1,62 @@
-﻿using Loading_Related_Data_Explicit_Loading.Configurations;
+﻿
 using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
 using System.Reflection;
-using System.Reflection.Emit;
-
 ApplicationDbContext context = new();
-#region Loading Related Data
+#region Explicit Loading
 
-#region Eager Loading
-//Eager Loading, a method that loads the main entity and its related entities in a single query. 
-#region Include
-// Include method is used to load related entities in the same query.
+//An approach that allows the data to be loaded according to conditions/needs to be added to the created query.
 
-////var employees = await context.Employees.Include("Orders").ToListAsync();
-//var employees = await context.Employees
-//    .Include(e=> e.Orders)
-//    .Where(e => e.Orders.Count > 2)
-//    .Include(e=> e.Region)
-//    .ToListAsync();
-//// in this sceneario it does not matter the order of Include method. If we change the order of Include or where methods, the result will be the same. 
-#endregion
-#region ThenInclude
-//var orders = await context.Orders
-//    .Include(o => o.Employee)
-//    .Include(o=>o.Employee.Region)
-//    .ToListAsync();
+//var employee=await context.Employees.FirstOrDefaultAsync(e => e.Id == 1);
 
-//ThenInclude method is used to load related entities of related entities.
+//if (employee.Name == "Alperen") 
+//{ 
+// var orders= await context.Orders.Where(o=> o.EmployeeId == employee.Id).ToListAsync();
+//}
+#region Reference
 
+//In the Explicit Loading process, if the navigation property of the table to be added to the query relationally is a singular type, we can add this table to the query with reference.
 
-//var regions = await context.Regions
-//    .Include(r => r.Employees)
-//    .ThenInclude(e => e.Orders)
-//    .ToListAsync();
-#endregion
-#region Filtered Include
-//In querying , we can filter the related entities with the Where method.  
+var employee = await context.Employees.FirstOrDefaultAsync(e => e.Id == 2);
 
-//var regions = await context.Regions.Include(r => r.Employees.Where(e => e.Name.Contains("a")).OrderByDescending(e=> e.Surname)).ToListAsync();
-
-//Supported Methods: where, OrderBy, Thenby, Skip, Take
-
-//If change tracker is enabled, the filtered include method will be applied to the entities that are already loaded in the context. It causes to load all entities from the database. If we want to apply the filtered include method to the entities that are already loaded in the context, we should use AsNoTracking method.
-#endregion
-#region Critical Info for Eager Loading
-//EF Core, can use the queries which  generated before 
-//var orders = await context.Orders.ToListAsync();
-
-//var employees= await context.Employees.ToListAsync();
-// It gives the employees order list from the memory. It does not make a query to the database.
-#endregion
-#region AutoInclude - EF Core 6
-//AutoInclude method is used to load related entities automatically. It is a new feature of EF Core 6.0.
-
-//var employees2 = await context.Employees.ToListAsync();
-
-#endregion
-#region IgnoreAutoIncludes
-// It provides to ignore the AutoInclude method for a specific query.
-
-//var employees3 = await context.Employees.IgnoreAutoIncludes().ToListAsync();
-#endregion
-#region Include Between Derived Entities
-
-#region Include with Cast Operand
-var persons1 = await context.Persons.Include(p => ((Employee)p).Orders).ToListAsync();
-#endregion
-#region Include with as Operand
-var persons2 = await context.Persons.Include(p => (p as Employee).Orders).ToListAsync();
-#endregion
-#region Include with 2. Overload 
-var persons3 = await context.Persons.Include("Orders").ToListAsync();
-#endregion
-#endregion
+//second query will be added to the main query
+await context.Entry(employee).Reference(e => e.Region).LoadAsync();
 
 Console.WriteLine();
 #endregion
 
-#region Explicit Loading
+#region Collection
 
-#region Collection Method
+//Explicit Loading sürecinde ilişkisel olarak sorguya eklenmek istenen tablonun navigation propertysi eğer ki çoğul/koleksiyonel bir türse bu tabloyu Collection ile sorguya ekleyebilemkteyiz.
 
-#endregion
-#region Reference Method
+//var employee = await context.Employees.FirstOrDefaultAsync(e => e.Id == 2);
+//...
+//...
+//...
+//await context.Entry(employee).Collection(e => e.Orders).LoadAsync();
 
-#endregion
-#endregion
-
-#region Lazy Loading
-
-#region N + 1 Problem
-
-#endregion
-#endregion
+//Console.WriteLine();
 #endregion
 
+#region Apply Aggregate operator on Collections
+//var employee = await context.Employees.FirstOrDefaultAsync(e => e.Id == 2);
+//...
+//...
+//...
+//var count = await context.Entry(employee).Collection(e => e.Orders).Query().CountAsync();
+Console.WriteLine();
+#endregion
+#region Apply Filter on Collections
+//var employee = await context.Employees.FirstOrDefaultAsync(e => e.Id == 2);
+////...
+////...
+////...
+//var orders = await context.Entry(employee).Collection(e => e.Orders).Query().Where(q => q.OrderDate.Day == DateTime.Now.Day).ToListAsync();
+#endregion
+#endregion
 
-
-public class Person
+public class Employee
 {
     public int Id { get; set; }
-
-}
-public class Employee : Person
-{
-    //public int Id { get; set; }
     public int RegionId { get; set; }
     public string? Name { get; set; }
     public string? Surname { get; set; }
@@ -131,19 +83,15 @@ public class Order
 
 class ApplicationDbContext : DbContext
 {
-    public DbSet<Person> Persons { get; set; }
     public DbSet<Employee> Employees { get; set; }
     public DbSet<Order> Orders { get; set; }
     public DbSet<Region> Regions { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
-        modelBuilder.Entity<Employee>()
-            .Navigation(e => e.Region)
-            .AutoInclude();
-
     }
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+
+protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlServer("Server=localhost, 1433;Database=ApplicationDb;User Id=sa;Password=Password1;TrustServerCertificate=True");
     }
