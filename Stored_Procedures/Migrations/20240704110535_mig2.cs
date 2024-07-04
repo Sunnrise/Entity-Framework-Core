0797ee6a805181f7367a9b07f23b22234042046b
Alperen Güneş <alperenmanavgat@outlook.com>
2024-07-04 14:37:25 +0300

    Stored Procedure initialize

---
+++ Stored_Procedures/Migrations/20240704110535_mig2.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Stored_Procedures.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($@"
+                CREATE PROCEDURE sp_GetPersonOrders
+                AS
+                    SELECT p.Name, COUNT(*) [Count] FROM Persons p
+                    JOIN Orders o 
+                        ON p.PersonId = o.PersonId
+                    GROUP BY p.Name
+                    ORDER BY COUNT(*) DESC
+                ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP PROCEDURE sp_GetPersonOrders");
+        }
+    }
+}
---
+++ Stored_Procedures/Program.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection;
+
+ApplicationDbContext context = new();
+#region What is Stored Procedure?
+//Stored Procedures, are a set of SQL statements that are stored on the database server and can be executed on the database server. Stored procedures are used to encapsulate a set of operations or queries to execute on the database server. Stored procedures can accept input parameters and return multiple values in the form of output parameters to the calling program or script.
+// Views act as virtual tables, but stored procedures act as virtual functions. Stored procedures can accept input parameters and return multiple values in the form of output parameters to the calling program or script.
+
+#endregion
+
+#region Stored Procedure Using with Entity Framework Core
+
+#region Stored Procedure Creation
+//1. Step: Create a empty migration
+//2. Step: Configure the stored procedure in the migration file with up and down method
+//3. Step: Update the database
+#endregion
+#region using Stored Procedure 
+// For using stored procedure in Entity Framework Core, we can use the following methods.
+// We create a entity and dbset  for the stored procedure and use the entity to get the data from the stored procedure.
+// then we use dbset property and then FromSql method to get the data from the stored procedure.
+#region FromSql
+var datas = await context.PersonOrders.FromSqlRaw($"EXECUTE sp_GetPersonOrders").ToListAsync();
+#endregion
+#endregion
+#region Return value Stored Procedure using
+
+#endregion
+
+#region Parameters with Stored Procedure 
+#region Input Parameters with  Stored Procedure
+
+#endregion
+
+#region Output Parameters with Stored Procedure
+
+#endregion
+
+#endregion
+#endregion
+Console.WriteLine();
+public class Person
+{
+    public int PersonId { get; set; }
+    public string Name { get; set; }
+
+    public ICollection<Order> Orders { get; set; }
+}
+public class Order
+{
+    public int OrderId { get; set; }
+    public int PersonId { get; set; }
+    public string Description { get; set; }
+
+    public Person Person { get; set; }
+}
+[NotMapped]
+public class PersonOrder
+{
+    public string Name { get; set; }
+    public int Count { get; set; }
+}
+class ApplicationDbContext : DbContext
+{
+    public DbSet<Person> Persons { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<PersonOrder> PersonOrders { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        modelBuilder.Entity<PersonOrder>().HasNoKey() ;
+
+        modelBuilder.Entity<Person>()
+            .HasMany(p => p.Orders)
+            .WithOne(o => o.Person)
+            .HasForeignKey(o => o.PersonId);
+    }
+
+protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost, 1433;Database=ApplicationDb;User Id=sa;Password=Password1;TrustServerCertificate=True");
+    }
+}
+
+
