4335081b88522069cfe290ca124d153bebb1fb53
Alperen Güneş <alperenmanavgat@outlook.com>
2024-07-04 19:55:23 +0300

    Scalar and Inline functions

---
+++ Functions/Program.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection;
+
+ApplicationDbContext context = new();
+#region What is the Scalar Functions ?
+//It is a function that returns a value.  
+#endregion
+#region Initialize Scalar Function
+//1. step : create a blank migration.
+//2. step : configure the function in the migration. up and down methods.
+//3. step : migration sent to the database.
+#endregion
+#region Integrate Scalar Function to the  EF Core
+
+#region HasDbFunction
+// HasDbFunction method is used to bind any function at the database level to a method in EF Core/software.
+#endregion
+
+var persons = await (from person in context.Persons
+                     where context.GetPersonTotalOrderPrice(person.PersonId) > 500
+                     select person).ToListAsync();
+
+Console.WriteLine();
+
+#endregion
+
+#region What is the Inline Functions?
+//It returns a table not a value.
+#endregion
+#region Inline Function Create
+//1. step : create a blank migration.
+//2. step : configure the function in the migration. up and down methods.
+//3. step : migration sent to the database.
+#endregion
+#region Integrate Inline Function to the EF Core
+
+#endregion
+
+public class Person
+{
+    public int PersonId { get; set; }
+    public string Name { get; set; }
+
+    public ICollection<Order> Orders { get; set; }
+}
+public class Order
+{
+    public int OrderId { get; set; }
+    public int PersonId { get; set; }
+    public string Description { get; set; }
+    public int Price { get; set; }
+
+    public Person Person { get; set; }
+}
+public class BestSellingStaff
+{
+    public string Name { get; set; }
+    public int OrderCount { get; set; }
+    public int TotalOrderPrice { get; set; }
+}
+class ApplicationDbContext : DbContext
+{
+    public DbSet<Person> Persons { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        #region Scalar
+        modelBuilder.HasDbFunction(typeof(ApplicationDbContext).GetMethod(nameof(ApplicationDbContext.GetPersonTotalOrderPrice), new[] { typeof(int) }))
+            .HasName("getPersonTotalOrderPrice");
+        #endregion
+        #region Inline
+        modelBuilder.HasDbFunction(typeof(ApplicationDbContext).GetMethod(nameof(ApplicationDbContext.BestSellingStaff), new[] { typeof(int) }))
+            .HasName("bestSellingStaff");
+
+        modelBuilder.Entity<BestSellingStaff>()
+            .HasNoKey();
+        #endregion
+
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        modelBuilder.Entity<Person>()
+            .HasMany(p => p.Orders)
+            .WithOne(o => o.Person)
+            .HasForeignKey(o => o.PersonId);
+    }
+
+
+    #region Scalar Functions
+    public int GetPersonTotalOrderPrice(int personId)
+        => throw new Exception();
+    #endregion
+    #region Inline Functions
+    public IQueryable<BestSellingStaff> BestSellingStaff(int totalOrderPrice = 10000)
+         => FromExpression(() => BestSellingStaff(totalOrderPrice));
+    #endregion
+
+protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost, 1433;Database=ApplicationDb;User Id=sa;Password=Password1;TrustServerCertificate=True");
+    }
+}
+
+
